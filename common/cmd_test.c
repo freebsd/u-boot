d672a1b7a81f53f1f6106c288ad7c2b23e5ca95e
M. Warner Losh <imp@freebsd.org>
2016-09-05 22:33:07 -0600

    Fix -z for null arguments

--- common/cmd_test.c
+++ common/cmd_test.c
@@ -56,9 +56,14 @@ static int do_test(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 	char * const *ap;
 	int i, op, left, adv, expr, last_expr, last_unop, last_binop;
 
-	/* args? */
-	if (argc < 3)
+	/*
+	 * If no args, that's bogus, return false.
+	 * If op is -z and no other args, answer is Yes, string is empty.
+	 */
+	if (argc < 2)
 		return 1;
+	else if (argc == 2)
+		return !(strcmp(argv[1], "-z") == 0);
 
 #ifdef DEBUG
 	{
